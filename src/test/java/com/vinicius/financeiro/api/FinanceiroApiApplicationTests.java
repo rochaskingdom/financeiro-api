b019440916cdb30d43be2eced60e4bf3c66145b0
Vinicius Rodrigues Rocha <viniciusr.rocha@outlook.com>
2020-04-11 19:54:39 -0300

    Projeto iniciado

---
+++ src/test/java/com/vinicius/financeiro/api/FinanceiroApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vinicius.financeiro.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinanceiroApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
