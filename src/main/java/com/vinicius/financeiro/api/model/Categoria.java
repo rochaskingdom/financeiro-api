f01647033026cc3a7886360c8e0154ce9b924cc9
Vinicius Rodrigues Rocha <viniciusr.rocha@outlook.com>
2020-04-12 17:21:33 -0300

    Validando valores inválidos com Bean Validation

--- src/main/java/com/vinicius/financeiro/api/model/Categoria.java
+++ src/main/java/com/vinicius/financeiro/api/model/Categoria.java
@@ -6,6 +6,7 @@ import lombok.Setter;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 @Getter
 @Setter
@@ -21,6 +22,7 @@ public class Categoria {
     private long id;
 
     @NotNull
+    @Size(min = 3, max = 20)
     @Column(name = "NOME")
     private String nome;
 
