df345829d76bea53403976b0b053335f774ea609
Vinicius Rodrigues Rocha <viniciusr.rocha@outlook.com>
2020-04-12 16:53:29 -0300

    Tratando erros com ExceptionHandler

---
+++ src/main/java/com/vinicius/financeiro/api/exceptionhandler/FinanceiroExceptionHandler.java
@@ -0,0 +1,40 @@
+package com.vinicius.financeiro.api.exceptionhandler;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@ControllerAdvice
+public class FinanceiroExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @Autowired
+    private MessageSource messageSource;
+
+    @Override
+    protected ResponseEntity<Object> handleHttpMessageNotReadable(
+            HttpMessageNotReadableException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+
+        String mensagemUsuario = messageSource.getMessage("mensagem.invalida", null, LocaleContextHolder.getLocale());
+        String mensagemDesenvolvedor = ex.getCause().toString();
+        return handleExceptionInternal(ex, new Erro(mensagemUsuario, mensagemDesenvolvedor), headers, HttpStatus.BAD_REQUEST, request);
+    }
+
+    @AllArgsConstructor
+    @Getter
+    @Setter
+    public static class Erro {
+
+        private String mensagemUsuario;
+        private String mensagemDesenvolvedor;
+    }
+}
