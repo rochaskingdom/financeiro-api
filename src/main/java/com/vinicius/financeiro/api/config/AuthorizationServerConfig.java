d605a094ec7383ce89d19582d2f4a4e0672fde9d
Vinicius Rodrigues Rocha <viniciusr.rocha@outlook.com>
2020-05-13 19:27:39 -0300

    Alternando OAuth 2 e Basic Security com profiles

--- src/main/java/com/vinicius/financeiro/api/config/AuthorizationServerConfig.java
+++ src/main/java/com/vinicius/financeiro/api/config/AuthorizationServerConfig.java
@@ -4,6 +4,7 @@ import com.vinicius.financeiro.api.config.token.CustomTokenEnhancer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
@@ -18,6 +19,7 @@ import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 
 import java.util.Arrays;
 
+@Profile("oauth-security")
 @Configuration
 @EnableAuthorizationServer
 public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdapter {
